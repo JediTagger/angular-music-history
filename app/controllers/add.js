cd37fc5e014d8876f13d4570137d8001686be3ef
Jesse Turner <jesseturner@comcast.net>
2015-08-26 16:08:52 -0500

    unique artist pull down functional

---
+++ app/controllers/add.js
@@ -0,0 +1,2 @@
+app.controller("AddCtrl", function($scope, $q) {
+});
--- app/controllers/song.js
+++ app/controllers/song.js
@@ -1,3 +1,56 @@
-app.controller("SongCtrl", function($scope) {
+app.controller("SongCtrl", function($scope, $q) {
+  $scope.songs = [];
+  $scope.secondSongs = [];
+  $scope.theSong = "";
+
+  function getFirstSongs() {
+    return $q(function(resolve, reject) {
+      $.ajax({
+        url: "./data/first.json"
+      })
+      .done(function(response) {
+        resolve(response.songs);
+      })
+      .fail(function(xhr, status, error) {
+        reject(error);
+      });
+    });
+  }
+
+  getFirstSongs()
+    .then(function(data) {
+      $scope.songs = data;
+    },function(error) {
+      console.log("error is: ", error);
+    });
+  
+  function getSecondSongs() {
+    return $q(function(resolve, reject) {
+      $.ajax({
+        url: "./data/second.json"
+      })
+      .done(function(response) {
+        resolve(response.songs);
+      })
+      .fail(function(xhr, status, error) {
+        reject(error);
+      });
+    });
+  }
+
+  getSecondSongs()
+    .then(function(data) {
+      $scope.secondSongs = data;
+      $.merge($scope.songs, $scope.secondSongs);
+    },function(error) {
+      console.log("error is: ", error);
+    });
+
+  $scope.killSong = function(song) {
+    var songIndex = $scope.songs.indexOf(song);
+    if (songIndex >= 0) {
+      $scope.songs.splice(songIndex, 1);
+    }
+  };
 
 });
